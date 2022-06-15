cc072c709b2217104d55c584e9cde6f9a4ac1ef9
Andre Pereira Fonseca <fonseca-andre19@hotmail.com>
2022-06-15 13:28:06 -0300

    Category not found

--- sneakers/views.py
+++ sneakers/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from utils.sneakers.factory import make_sneaker
 
@@ -6,15 +6,22 @@ from sneakers.models import Sneaker
 
 
 def home(request):
-    sneakers = Sneaker.objects.all().order_by('-id')
+    sneakers = Sneaker.objects.filter(is_published=True).order_by('-id')
     return render(request, 'sneakers/pages/home.html', context={
         'sneakers': sneakers,
     })
 
+
+
 def category(request, category_id):
-    sneakers = Sneaker.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'sneakers/pages/home.html', context={
+    sneakers = Sneaker.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+
+    if not sneakers:
+        raise Http404('Not found')
+
+    return render(request, 'sneakers/pages/category.html', context={
         'sneakers': sneakers,
+        'title': f'{sneakers.first().category.name} - Category |'
     })
 
 def sneaker(request, id):
