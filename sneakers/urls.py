58bfe3a9ae927bdc4ef3feb9699dafeaa20b4d20
Andre Pereira Fonseca <fonseca-andre19@hotmail.com>
2022-06-26 10:26:32 -0300

    running tests

--- sneakers/urls.py
+++ sneakers/urls.py
@@ -7,7 +7,6 @@ app_name = 'sneakers'
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('sneakers/category/<int:category_id>/', views.category, name="category"),
+    path('sneakers/category/<int:category_id>/', views.category, name="category"),  # noqa: E501
     path('sneakers/<int:id>/', views.sneaker, name="sneaker"),
-    
 ]
