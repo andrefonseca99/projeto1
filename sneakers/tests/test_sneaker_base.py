a1d115a352b8676dbdc0d42c57e28940e42b54e9
Andre Pereira Fonseca <fonseca-andre19@hotmail.com>
2022-06-29 15:04:28 -0300

    More tests

---
+++ sneakers/tests/test_sneaker_base.py
@@ -0,0 +1,63 @@
+from django.test import TestCase
+from sneakers.models import Category, Sneaker, User
+
+
+class SneakerTestBase(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def make_category(self, name='Category'):
+        return Category.objects.create(name=name)
+
+    def make_author(
+        self,
+        first_name='user',
+        last_name='name',
+        username='username',
+        password='123456',
+        email='username@email.com'
+    ):
+        return User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            password=password,
+            email=email
+        )
+
+    def make_sneaker(
+        self,
+        category_data=None,
+        author_data=None,
+        title='Sneaker Title',
+        description='Sneaker Description',
+        slug='sneaker-slug',
+        condition_value=10,
+        condition_unit='/10',
+        price=100,
+        price_unit='R$',
+        sneaker_description='Sneaker condition description',
+        sneaker_description_is_html=False,
+        is_published=True,
+        cover='media\\sneakers\\covers\2022\06\15\\WhatsApp_Image_2022-06-15_at_11.24.14.jpeg'  # noqa: E501
+    ):
+        if category_data is None:
+            category_data = {}
+
+        if author_data is None:
+            author_data = {}
+        return Sneaker.objects.create(
+            category=self.make_category(**category_data),
+            author=self.make_author(**author_data),
+            title=title,
+            description=description,
+            slug=slug,
+            condition_value=condition_value,
+            condition_unit=condition_unit,
+            price=price,
+            price_unit=price_unit,
+            sneaker_description=sneaker_description,
+            sneaker_description_is_html=sneaker_description_is_html,
+            is_published=is_published,
+            cover=cover
+        )
--- sneakers/tests/test_sneaker_view.py
+++ sneakers/tests/test_sneaker_view.py
@@ -1,76 +1,59 @@
-from django.test import TestCase
 from django.urls import resolve, reverse
 from sneakers import views
-from sneakers.models import Category, Sneaker, User
 
-# Create your tests here.
+from .test_sneaker_base import SneakerTestBase
 
 
-class SneakerViewsTest(TestCase):
+class SneakerViewsTest(SneakerTestBase):
 
     def test_sneaker_home_view_function_is_correct(self):
         view = resolve(reverse('sneakers:home'))
+
         self.assertIs(view.func, views.home)
 
     def test_sneaker_home_view_return_status_code_200_OK(self):
         response = self.client.get(reverse('sneakers:home'))
+
         self.assertEqual(response.status_code, 200)
 
     def test_sneaker_home_view_loads_correct_template(self):
         response = self.client.get(reverse('sneakers:home'))
+
         self.assertTemplateUsed(response, 'sneakers/pages/home.html')
 
     def test_sneaker_home_shows_no_sneakers_found_if_no_sneakers(self):
         response = self.client.get(reverse('sneakers:home'))
+
         self.assertIn('No sneakers found', response.content.decode('utf-8'))
 
     def test_sneaker_home_template_loads_sneakers(self):
-        category = Category.objects.create(name='Category')
-        author = User.objects.create_user(
-            first_name='user',
-            last_name='name',
-            username='username',
-            password='123456',
-            email='username@email.com',
-        )
-        sneaker = Sneaker.objects.create(
-            category=category,
-            author=author,
-            title='Sneaker Title',
-            description='Sneaker Description',
-            slug='sneaker-slug',
-            condition_value=10,
-            condition_unit='/10',
-            price=100,
-            price_unit='R$',
-            sneaker_description='Sneaker condition description',
-            sneaker_description_is_html=False,
-            is_published=True,
-            cover='media\\sneakers\\covers\2022\06\15\\WhatsApp_Image_2022-06-15_at_11.24.14.jpeg'  # noqa: E501
-        )
+        # We need a sneaker for this test
+        self.make_sneaker()
 
-        sneaker = sneaker
         response = self.client.get(reverse('sneakers:home'))
         content = response.content.decode('utf-8')
         response_context_sneakers = response.context['sneakers']
+
+        # Check if one sneaker exists
         self.assertIn('Sneaker Title', content)
-        self.assertIn('Sneaker Description', content)
-        self.assertIn('10/10', content)
-        self.assertIn('R$ 100', content)
         self.assertEqual(len(response_context_sneakers), 1)
 
     def test_sneaker_category_view_function_is_correct(self):
         view = resolve(reverse('sneakers:category', kwargs={'category_id': 1000}))  # noqa: E501
+
         self.assertIs(view.func, views.category)
 
     def test_sneaker_category_view_return_404_if_no_sneakers_found(self):
         response = self.client.get(reverse('sneakers:category', kwargs={'category_id': 1000}))  # noqa: E501
+
         self.assertEqual(response.status_code, 404)
 
     def test_sneaker_detail_view_function_is_correct(self):
         view = resolve(reverse('sneakers:sneaker', kwargs={'id': 1000}))
+
         self.assertIs(view.func, views.sneaker)
 
     def test_sneaker_detail_view_return_404_if_no_sneakers_found(self):
         response = self.client.get(reverse('sneakers:sneaker', kwargs={'id': 1000}))  # noqa: E501
+
         self.assertEqual(response.status_code, 404)
