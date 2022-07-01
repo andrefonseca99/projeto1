56d1066768be781d8c8a2ebea782d8f53d4c78ba
Andre Pereira Fonseca <fonseca-andre19@hotmail.com>
2022-06-30 22:47:02 -0300

    more view tests

--- sneakers/tests/test_sneaker_view.py
+++ sneakers/tests/test_sneaker_view.py
@@ -40,6 +40,15 @@ class SneakerViewsTest(SneakerTestBase):
         response = self.client.get(reverse('sneakers:category', kwargs={'category_id': 1000}))  # noqa: E501
         self.assertEqual(response.status_code, 404)
 
+    def test_sneaker_category_template_loads_sneakers(self):
+        needed_title = 'This is a category text'
+        # We need a sneaker for this test
+        self.make_sneaker(title=needed_title)
+        response = self.client.get(reverse('sneakers:category', kwargs={'category_id': 1}))  # noqa: E501
+        content = response.content.decode('utf-8')
+        # Check if one sneaker exists
+        self.assertIn(needed_title, content)
+
     def test_sneaker_detail_view_function_is_correct(self):
         view = resolve(reverse('sneakers:sneaker', kwargs={'id': 1000}))
         self.assertIs(view.func, views.sneaker)
@@ -47,3 +56,12 @@ class SneakerViewsTest(SneakerTestBase):
     def test_sneaker_detail_view_return_404_if_no_sneakers_found(self):
         response = self.client.get(reverse('sneakers:sneaker', kwargs={'id': 1000}))  # noqa: E501
         self.assertEqual(response.status_code, 404)
+
+    def test_sneaker_detail_template_loads_the_correct_sneaker(self):
+        needed_title = 'This is a detail page that loads one sneaker'
+        # We need a sneaker for this test
+        self.make_sneaker(title=needed_title)
+        response = self.client.get(reverse('sneakers:sneaker', kwargs={'id': 1}))  # noqa: E501
+        content = response.content.decode('utf-8')
+        # Check if one sneaker exists
+        self.assertIn(needed_title, content)
