a15628a7ba29e601ee520233bd9eb34f32b5276f
André Pereira Fonseca <fonseca-andre19@hotmail.com>
2022-07-21 10:37:20 -0300

    Creating authors app

--- project/settings.py
+++ project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # My apps
-    'sneakers'
+    'sneakers',
+    'authors',
 ]
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -23,6 +23,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sneakers.urls')),
+    path('authors/', include('authors.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
