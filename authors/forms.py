c84fb3f66485916c1cbd3a8075053cd51c96a6ac
André Pereira Fonseca <fonseca-andre19@hotmail.com>
2022-07-25 10:21:52 -0300

    using functions to add field placeholders

--- authors/forms.py
+++ authors/forms.py
@@ -2,7 +2,37 @@ from django import forms
 from django.contrib.auth.models import User
 
 
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    field.widget.attrs['placeholder'] = placeholder_val
+
+
 class RegisterForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Your username')
+        add_placeholder(self.fields['email'], 'Your e-mail')
+        add_placeholder(self.fields['first_name'], 'Your first name')
+        add_placeholder(self.fields['last_name'], 'Your last name')
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Your password',
+        }),
+    )
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password here',
+        }),
+    )
+
     class Meta:
         model = User
         fields = [
