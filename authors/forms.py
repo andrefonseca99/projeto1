4b90756707ac2513cca24e4e633aea75a88a6ea0
André Pereira Fonseca <fonseca-andre19@hotmail.com>
2022-07-25 10:02:00 -0300

    adding widgets, labels, error_messages to forms

--- authors/forms.py
+++ authors/forms.py
@@ -11,4 +11,40 @@ class RegisterForm(forms.ModelForm):
                 'username',
                 'email',
                 'password'
-                ]
+        ]
+        # exclude = ['last_name']
+        labels = {
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'username': 'Username',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+        help_texts = {
+            'password': None,
+            'username': None,
+        }
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+                'max_lenght': 'This field must have less than 3 charcters',
+            }
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your first name here',
+                # 'class': 'input text_input',
+            }),
+            'last_name': forms.TextInput(attrs={
+                'placeholder': 'Type your last name here',
+            }),
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Type your username here',
+            }),
+            'email': forms.TextInput(attrs={
+                'placeholder': 'Type your e-mail here',
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here',
+            }),
+        }
