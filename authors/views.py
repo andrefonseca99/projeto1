1fc56581252e4697c455c2b7f039260422a9942f
André Pereira Fonseca <fonseca-andre19@hotmail.com>
2022-07-29 11:16:20 -0300

    Correcting password cryptography

--- authors/views.py
+++ authors/views.py
@@ -23,8 +23,11 @@ def register_create(request):
     form = RegisterForm(POST)
 
     if form.is_valid():
-        form.save()
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
         messages.success(request, 'Your user is created, please log in.')
+
         del(request.session['register_form_data'])
 
     return redirect('authors:register')
