36522113e145990a823d42586414ba6fca0acabf
André Pereira Fonseca <fonseca-andre19@hotmail.com>
2022-08-08 08:25:37 -0300

    Organinzing functional tests

--- tests/functional_tests/sneakers/base.py
+++ tests/functional_tests/sneakers/base.py
@@ -1,7 +1,6 @@
 import time
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.webdriver.common.by import By
 from utils.browser import make_chrome_browser
 
 
@@ -17,11 +16,3 @@ class SneakerBaseFunctionalTest(StaticLiveServerTestCase):
 
     def sleep(self, seconds=5):
         time.sleep(seconds)
-
-
-class SneakerHomePageFunctionalTest(SneakerBaseFunctionalTest):
-
-    def test_sneaker_home_page_without_sneakers_error_message(self):
-        self.browser.get(self.live_server_url)
-        body = self.browser.find_element(By.TAG_NAME, 'body')
-        self.assertIn('No sneakers found', body.text)
---
+++ tests/functional_tests/sneakers/test_sneaker_home_page.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+from .base import SneakerBaseFunctionalTest
+
+
+class SneakerHomePageFunctionalTest(SneakerBaseFunctionalTest):
+
+    def test_sneaker_home_page_without_sneakers_error_message(self):
+        self.browser.get(self.live_server_url)
+        body = self.browser.find_element(By.TAG_NAME, 'body')
+        self.assertIn('No sneakers found', body.text)
