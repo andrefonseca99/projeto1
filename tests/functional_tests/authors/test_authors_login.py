057ebed7d7806f79d7209ffd81271806ab232ff2
André Pereira Fonseca <fonseca-andre19@hotmail.com>
2022-08-12 17:13:35 -0300

    Testing login form

---
+++ tests/functional_tests/authors/test_authors_login.py
@@ -0,0 +1,37 @@
+import pytest
+from django.contrib.auth.models import User
+from django.urls import reverse
+from selenium.webdriver.common.by import By
+
+from .base import AuthorsBaseTest
+
+
+@pytest.mark.functional_test
+class AuthorsLoginTest(AuthorsBaseTest):
+
+    def test_user_valid_data_can_login_successfully(self):
+        string_password = 'pass'
+        user = User.objects.create_user(
+            username='my_user', password=string_password
+        )
+
+        # User open login page
+        self.browser.get(self.live_server_url + reverse('authors:login'))
+
+        # User looks for login form
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+        username_field = self.get_by_placeholder(form, 'Type your username')
+        password_field = self.get_by_placeholder(form, 'Type your password')
+
+        # User types his username and password
+        username_field.send_keys(user.username)
+        password_field.send_keys(string_password)
+
+        # User sends form
+        form.submit()
+
+        # User sees successful message and his user
+        self.assertIn(
+            f'You are logged in with {user.username}',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
