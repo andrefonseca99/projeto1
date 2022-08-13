3a0e6e60c900139636dffcc320329f63586ddc3a
André Pereira Fonseca <fonseca-andre19@hotmail.com>
2022-08-13 12:17:01 -0300

    Test sending POST to login_create

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -35,3 +35,13 @@ class AuthorsLoginTest(AuthorsBaseTest):
             f'You are logged in with {user.username}',
             self.browser.find_element(By.TAG_NAME, 'body').text
         )
+
+    def test_login_create_raises_404_if_not_POST_method(self):
+        self.browser.get(
+            self.live_server_url + reverse('authors:login_create')
+        )
+
+        self.assertIn(
+            'Not Found',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
