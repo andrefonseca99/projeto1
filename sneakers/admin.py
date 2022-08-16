3f0a668458d88fde1609c3b4452c96d8e6933a1c
André Pereira Fonseca <fonseca-andre19@hotmail.com>
2022-08-16 15:48:28 -0300

    adjusting django admin area

--- sneakers/admin.py
+++ sneakers/admin.py
@@ -10,10 +10,15 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 @admin.register(Sneaker)
-
-
 class SneakerAdmin(admin.ModelAdmin):
-    ...
+    list_display = ['id', 'title', 'created_at', 'is_published', 'author']
+    list_display_links = 'title', 'created_at',
+    search_fields = 'id', 'title', 'description', 'slug', 'sneaker_description'
+    list_filter = 'category', 'author', 'is_published', \
+        'sneaker_description_is_html'
+    list_per_page = 10
+    list_editable = 'is_published',
+    ordering = '-id',
 
 
 admin.site.register(Category, CategoryAdmin)
