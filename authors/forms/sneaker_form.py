09d1adb703e4f252896338d4c2aab1e50965f992
André Pereira Fonseca <fonseca-andre19@hotmail.com>
2022-08-18 13:40:53 -0300

    editing edit form layout

--- authors/forms/sneaker_form.py
+++ authors/forms/sneaker_form.py
@@ -13,3 +13,24 @@ class AuthorsSneakerForm(forms.ModelForm):
         model = Sneaker
         fields = 'title', 'description', 'condition_value', 'price', \
             'sneaker_description', 'cover'
+        widgets = {
+            'cover': forms.FileInput(
+                attrs={
+                    'class': 'span-2'
+                }
+            ),
+            'condition_value': forms.Select(
+                choices=(
+                    ('10', '10/10'),
+                    ('9', '9/10'),
+                    ('8', '8/10'),
+                    ('7', '7/10'),
+                    ('6', '6/10'),
+                    ('5', '5/10'),
+                    ('4', '4/10'),
+                    ('3', '3/10'),
+                    ('2', '2/10'),
+                    ('1', '1/10'),
+                )
+            )
+        }
