6fa625678966ad5256f01d3474e4ac4a3ab659b2
André Pereira Fonseca <fonseca-andre19@hotmail.com>
2022-08-18 13:22:23 -0300

    Changing dashboard_sneaker form css

--- authors/forms/sneaker_form.py
+++ authors/forms/sneaker_form.py
@@ -1,8 +1,14 @@
 from django import forms
 from sneakers.models import Sneaker
+from utils.django_forms import add_attr
 
 
 class AuthorsSneakerForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_attr(self.fields.get('sneaker_description'), 'class', 'span-2')
+        add_attr(self.fields.get('cover'), 'class', 'span-2')
+
     class Meta:
         model = Sneaker
         fields = 'title', 'description', 'condition_value', 'price', \
