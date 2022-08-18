d02d9e2c3f58fc1e4da29017ca65dd52b6281c2f
André Pereira Fonseca <fonseca-andre19@hotmail.com>
2022-08-18 16:02:07 -0300

    Validating sneaker form

--- authors/forms/sneaker_form.py
+++ authors/forms/sneaker_form.py
@@ -1,4 +1,7 @@
+from collections import defaultdict
+
 from django import forms
+from django.core.exceptions import ValidationError
 from sneakers.models import Sneaker
 from utils.django_forms import add_attr
 
@@ -6,6 +9,9 @@ from utils.django_forms import add_attr
 class AuthorsSneakerForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self._my_errors = defaultdict(list)
+
         add_attr(self.fields.get('sneaker_description'), 'class', 'span-2')
         add_attr(self.fields.get('cover'), 'class', 'span-2')
 
@@ -34,3 +40,44 @@ class AuthorsSneakerForm(forms.ModelForm):
                 )
             )
         }
+
+    def clean(self, *args, **kwargs):
+        super_clean = super().clean(*args, **kwargs)
+        cleaned_data = self.cleaned_data
+
+        title = cleaned_data.get('title')
+        description = cleaned_data.get('description')
+        price = cleaned_data.get('price')
+        sneaker_description = cleaned_data.get('sneaker_description')
+
+        if len(title) < 5:
+            self._my_errors['title'].append(
+                'Title must have at least 5 characthers'
+            )
+
+        if title == description:
+            self._my_errors['title'].append(
+                'Title needs to be different from description'
+            )
+            self._my_errors['description'].append(
+                'Description needs to be different from title'
+            )
+
+        if price < 5:
+            self._my_errors['price'].append(
+                'Sneaker minimum price is R$5,00'
+            )
+        if price > 100000:
+            self._my_errors['price'].append(
+                'Sneaker maximum price is R$100.000,00'
+            )
+
+        if len(sneaker_description) < 30:
+            self._my_errors['sneaker_description'].append(
+                'Sneaker description must have at least 30 charachters'
+            )
+
+        if self._my_errors:
+            raise ValidationError(self._my_errors)
+
+        return super_clean
