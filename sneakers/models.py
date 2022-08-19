85c1ab9b06ff24b07875a5590f7cd6793c8efae4
André Pereira Fonseca <fonseca-andre19@hotmail.com>
2022-08-19 10:21:49 -0300

    Making unique slugs

--- sneakers/models.py
+++ sneakers/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
+from django.utils.crypto import get_random_string
+from django.utils.text import slugify
 
 
 class Category(models.Model):
@@ -14,17 +17,29 @@ class Sneaker(models.Model):
     description = models.CharField(max_length=165)
     slug = models.SlugField(unique=True)
     condition_value = models.IntegerField()
-    condition_unit = models.CharField(max_length=65)
+    condition_unit = models.CharField(default='/10', max_length=65)
     price = models.IntegerField()
-    price_unit = models.CharField(max_length=65)
+    price_unit = models.CharField(default='R$', max_length=65)
     sneaker_description = models.TextField()
     sneaker_description_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='sneakers/covers/%Y/%m/%d/')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)  # noqa: E501
+    category = models.ForeignKey(Category, default=1, on_delete=models.SET_NULL, null=True)  # noqa: E501
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('sneakers:sneaker', args=(self.id,))
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f'{slugify(self.title)}'
+            while Sneaker.objects.filter(slug=slug).exists():
+                slug = slug + '-' + get_random_string(length=4)
+            self.slug = slug
+
+        return super().save(*args, **kwargs)
