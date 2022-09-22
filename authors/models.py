ad4b00d1f016ee1cb180a849fbb6fe1607078264
André Pereira Fonseca <andrepfonseca19@gmail.com>
2022-09-22 16:50:29 -0300

    Creating Profile Model

--- authors/models.py
+++ authors/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=165, default='', blank=True)
+    contact = models.TextField(max_length=65, default='', blank=True)
